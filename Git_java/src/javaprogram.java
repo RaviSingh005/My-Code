4b5ea2b1285d51a6691206661cef16324a673e5f
sdac <sdac@sdac-PC>
2024-02-07 19:49:34 +0530

    Upload code

---
+++ Git_java/src/javaprogram.java
@@ -0,0 +1,11 @@
+
+public class javaprogram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Guys");
+		System.out.println("Hello Guys");
+		System.out.println("Hello Guys");
+		System.out.println("Hello Guys");
+	}
+
+}
